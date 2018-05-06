de269385a47ab648037ac158233a254f059f1afd
Mohammad Zulqurnain <mohammadzulqurnain@Mohammads-MacBook-Pro.local>
2018-05-06 14:54:13 +0500

    YOLO implementation for Netvirta

---
+++ YOLOImplementation/Common/AppDelegate.swift
@@ -0,0 +1,15 @@
+//AppDelegate.swift
+//
+//  Created by Mohammad Zulqurnain on 1/4/18.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
